064009b7a27ef1e012d4d7ca96b285678ff4b9d0
Parmod Kumar <yparmodyadav@gmail.com>
2023-10-21 19:04:22 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 // Module Imported
-import express from 'express';
+import express, { urlencoded } from 'express';
 import ejs from 'ejs';
 import ejsLayouts from 'express-ejs-layouts';
 import path from 'path';
 import UserController from './src/controllers/users.controllers.js';
 import RecruiterController from './src/controllers/recruiters.controller.js';
 import JobController from './src/controllers/jobs.controller.js';
+import {auth} from './middlewares/auth.middleware.js';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
 
 // Server create
 const app = express();
+
+// Cookies Parser
+app.use(cookieParser());
+
+// Session Create
+app.use(session({
+    secret : "SecretKey",
+    resave : false,
+    saveUninitialized : true,
+    cookie : ({secure : false}),
+}));
+
+// Parse Form Data
+app.use(express.urlencoded({extended : true}));
 
 // View engine
 app.set('view engine' , "ejs");
@@ -31,13 +48,16 @@ app.get('/',usersController.getHome);
 app.get('/jobs',jobsController.getJobs);
 
 // Recruiter routes
-app.get('/login',recruitersController.getLogin);
 app.get('/register',recruitersController.getRegister);
+app.post('/register',recruitersController.postRegister);
+app.get('/login',recruitersController.getLogin);
+app.post('/login',recruitersController.postLogin);
+app.get('/logout',recruitersController.logout);
 
 // Jobs
-app.get('/jobs/job-page',jobsController.getJobPage)
-app.get('/postjob',recruitersController.getPostJob)
-app.get('/applicants',recruitersController.getApplicants)
+app.get('/jobs/job-page',jobsController.getJobPage);
+app.get('/postjob',auth,recruitersController.getPostJob);
+app.get('/applicants',auth,recruitersController.getApplicants);
 
 
 
